d8b728e0e6cc3158b5c11bff01f8e5e35833ed72
Michał Perlak <michalperlak@windowslive.com>
2017-11-20 20:58:59 +0100

    Repo init

---
+++ KotlinDemoServer/src/main/kotlin/pl/edu/agh/eaiib/io/kotlindemo/Server.kt
@@ -0,0 +1,20 @@
+package pl.edu.agh.eaiib.io.kotlindemo
+
+import io.ktor.application.*
+import io.ktor.http.*
+import io.ktor.response.*
+import io.ktor.routing.*
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
+
+fun main(args: Array<String>) {
+
+    val server = embeddedServer(Netty, 8081) {
+        routing {
+            get("/") {
+                call.respondText("Hello, world!", ContentType.Text.Html)
+            }
+        }
+    }
+    server.start(wait = true)
+}
