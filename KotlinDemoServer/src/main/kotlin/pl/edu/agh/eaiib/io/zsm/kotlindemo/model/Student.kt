54f87960c0a640f61f34c5077c6f935c5d6ae109
Michał Perlak <michalperlak@windowslive.com>
2017-11-23 18:14:33 +0100

    Project reset

--- KotlinDemoServer/src/main/kotlin/pl/edu/agh/eaiib/io/zsm/kotlindemo/model/Student.kt
+++
@@ -1,9 +0,0 @@
-package pl.edu.agh.eaiib.io.zsm.kotlindemo.model
-
-import org.springframework.data.annotation.Id
-
-data class Student(@Id val id: String = "",
-                   val firstName: String = "",
-                   val lastName: String = "",
-                   val faculty: String = "",
-                   val fieldOfStudy: String = "")
